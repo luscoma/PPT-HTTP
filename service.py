655f2949992f74eaee42f087224a18a0cfaab316
Alex Lusco <luscoma@auburn.edu>
2011-03-25 11:42:54 -0500

    Fixed register method so it requires the name of the presentation and the number of slides then builds the presentation object accordingly

--- service.py
+++ service.py
@@ -16,22 +16,30 @@ class RegisterHandler(webapp.RequestHandler):
   # Gets a new session id generated for 
   # Use and types it
   # the secondary parameter is unused since we are retreving a new registration id
+  # It isn't required in the URL but it appengine complains if our method doesn't accept two params
   def post(self,unused):
-    # Create a new presentation and return the key
-    p = Presentation()
-    p.put()                                                         # creates a default basic presentation
-    self.response.out.write(p.key().id())                           # Writes just the id of the key so fanciness required
+    # Get the post parameters and ensure they are valid
+    name = self.request.get('name')                                 # Name of presentation
+    num_slides = self.request.get('slides')                         # Number of slides
+
+    if (name == '' or !num_slides.isdigit()):                       # If the parameters aren't correct then let them know
+      self.error(500)
+      self.response.out.write("The post parameters 'name' and 'slides' are required.  'slides' must also be a number")
+    else:
+      p = Presentation(Name=name, NumberSlides=num_slides)          # Create a new presentation
+      p.put()                                                       # creates a default basic presentation
+      self.response.out.write(p.key().id())                         # Writes just the id of the key, no fanciness required
 
   # Deletes a session id if exists
   # This will signify the end of a session
   def delete(self,pptId):
-    key = db.Key.from_path('Presentation',int(pptId))               # Get the key from the pptId (which we convert to integer)
-    presentation = db.get(key)
+    key = db.Key.from_path('Presentation',int(pptId))               # Get the key from the pptId (which we convert to integer, which we know is an integer due to the regex)
+    presentation = db.get(key)                                      # Retreive presentation
     if (presentation == None):                                      # If it doesn't exist
       self.error(404)                                               # we do a 404
     else:
-      presentation.delete()                                         
-      self.response.set_status(204)                                 # Otherwise, we delete it and send a 204 code which means no content 
+      presentation.delete()                                         # Delete the presentation
+      self.response.set_status(204)                                 # Send a 204 code which means ok, but no content
     
 
 # Handles the present page
@@ -46,7 +54,7 @@ class PresentHandler(webapp.RequestHandler):
     if (presentation == None):
       self.error(404)                                               # doesn't exist, out of luck send the 404
     else:
-      # Print the template
+      # Print the template (for now just debug)
       self.response.headers['Content-Type'] = 'text/plain'
       self.response.out.write("Present: ")
       self.response.out.write(pptId)
